8ac836cc1bf285d13511b98fee1dfa7cee29f819
Tien Nguyen <tien.nguyen@s3corp.com.vn>
2025-12-02 17:07:43 +0700

    props and reactivity

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,41 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Test from "./lib/Test.svelte";
+
+  let newPost = '';
+  let posts = [];
+  let error = '';
+  let characterLeft;
+  let characterLeftStyle;
+
+  $:characterLeft = 10 - newPost.length;
+  characterLeftStyle = 'color: blue';
+  $:if (characterLeft < 0) {
+    characterLeftStyle = 'color: red';
+  }
+
+  const addPost = () => {
+    if (characterLeft<0) {
+      error = 'Exceeded error!';
+      return;
+    }
+    posts = [...posts, newPost];
+    newPost = '';
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Soliloquy app</h1>
+  <h2>Social media without the sharing</h2>
+  <label>Talk to yourself: <input bind:value={newPost} /></label>
+  <span style={characterLeftStyle}>Character left: {characterLeft}</span>
+  <p>{error}</p>
+  <button on:click={addPost}>Post it</button>
+  {#each posts as post}
+    <div>{post}</div>
+  {/each}
+  <Test published={true} title="This is just a title" price={100} />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+  
 </style>
---
+++ src/lib/Test.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let published = false;
+  export let title = undefined;
+  export let price = 0;
+</script>
+{#if published}
+<p>The title is: {title}, price is ${price}</p>
+{:else}
+<p>The article is not published!</p>
+{/if}
+<p>This is a template</p>
