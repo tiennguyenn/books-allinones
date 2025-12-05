26013abfa7365a86c2ac5594b91c91b15a733292
Tien Nguyen <tien.nguyen@s3corp.com.vn>
2025-12-05 14:20:00 +0700

    event bubbling

---
+++ src/lib/MyButton.svelte
@@ -0,0 +1,11 @@
+<script>
+// @ts-nocheck
+
+  export let count = 0;
+</script>
+
+<div>
+  <button {count}>
+    <slot></slot>
+  </button>
+</div>
--- src/lib/Test.svelte
+++ src/lib/Test.svelte
@@ -1,7 +1,16 @@
 <script>
+  import MyButton from "./MyButton.svelte";
+
   export let published = false;
   export let title = undefined;
   export let price = 0;
+  
+  let message = '';
+  let buttonArray = [1,2,3,4,5,6,7,8,9,10];
+
+  function clickMe(e) {
+    message = `You click the button ${e.target.getAttribute('count')}`;
+  }
 </script>
 {#if published}
 <p>The title is: {title}, price is ${price}</p>
@@ -9,3 +18,8 @@
 <p>The article is not published!</p>
 {/if}
 <p>This is a template</p>
+
+{#each buttonArray as button}
+  <MyButton count={button} on:click={clickMe}>Click me</MyButton>
+{/each}
+<p>{message}</p>
